d16fa039bb8243904687ac1b3f937cb08881e945
Léo DESIGAUX <leodesigaux@gmail.com>
2018-06-14 15:09:04 +0200

    Setting example to work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
-import Wng from './wng/Wng.js'
+import Wng from './lib/Wng.js'
+
+// Easy selector
+const $ = selector => document.querySelector(selector)
+
+// Add line to logs
+const Log = (msg, err = false) => {
+  let classErr = ''
+  if (err) {
+    classErr = ' class="error"'
+  }
+  $('.logs').innerHTML = $('.logs').innerHTML + '<p' + classErr + '>' + msg + '</p>'
+}
+
+let WNG
 
 window.onload = () => {
-  document.querySelector('span').innerHTML = 'It work!'
-  window.wng = new Wng('test')
+  WNG = null
+  Log('Initialized...')
+}
+
+// TESTS
+$('#CLEAR').onclick = evt => {
+  evt.preventDefault()
+  $('.logs').innerHTML = ''
+}
+
+$('#SETKEY').onclick = evt => {
+  const $consumerKey = $('#KEY').value
+  try {
+    WNG = new Wng($consumerKey)
+    Log('WNG Object created')
+    Log('Endpoint: ' + WNG.endpoint)
+    $('#SETKEY').setAttribute('disabled', true)
+  } catch (err) {
+    Log(err, true)
+  }
 }
